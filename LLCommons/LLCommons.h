d02462fb9e0434b2d8ff387d04e21b8237ac5b02
Igor <igor.custodio8@gmail.com>
2021-09-16 18:45:57 -0300

    Initial Commit

---
+++ LLCommons/LLCommons.h
@@ -0,0 +1,18 @@
+//
+//  LLCommons.h
+//  LLCommons
+//
+//  Created by Igor Custodio on 16/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LLCommons.
+FOUNDATION_EXPORT double LLCommonsVersionNumber;
+
+//! Project version string for LLCommons.
+FOUNDATION_EXPORT const unsigned char LLCommonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LLCommons/PublicHeader.h>
+
+
